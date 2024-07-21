3ba25a8adbf696507f63003aec46175eaf328923
IvanIvanReg27 <makhorin0088@gmail.com>
2024-07-22 02:07:13 +1000

    test message_4

--- src/main/java/org/example/MainServlet.java
+++ src/main/java/org/example/MainServlet.java
@@ -16,7 +16,7 @@ public class MainServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
         PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Hello_2!");
+        printWriter.write("Hello_Aston!");
         printWriter.close();
     }
 }
